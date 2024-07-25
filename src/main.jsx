54cc95ab8bd6bca875f07c23330011d61fd968d3
lester <lesterc013@gmail.com>
2024-07-25 12:06:37 +0800

    Refactored store configuration into store.js from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 import { Provider } from 'react-redux'
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
+// import noteReducer from './reducers/noteReducer'
+// import filterReducer from './reducers/filterReducer'
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-})
+// const store = configureStore({
+//   reducer: {
+//     notes: noteReducer,
+//     filter: filterReducer,
+//   },
+// })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
